e632b99c2705cd9f3eaf3af3252e7479fc40853c
Abdullah Shahzad <abdullahhunjra@gmail.com>
2025-08-18 01:36:29 +0100

    Train job

--- cicd/run_training_job.py
+++ cicd/run_training_job.py
@@ -4,22 +4,21 @@ from datetime import datetime
 
 role = "arn:aws:iam::755283537318:role/telco-sagemaker-role"
 bucket = "telco-churn-prediction-bucket"
-entry_point_script = "train.py"
 
 timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 job_name = f"telco-training-{timestamp}"
 
 sklearn_estimator = SKLearn(
-    entry_point=entry_point_script,
+    entry_point="train.py",              # this means: scripts/train.py
+    source_dir="scripts",                # folder where train.py is
     role=role,
+    instance_count=1,
     instance_type="ml.m5.large",
     framework_version="0.23-1",
-    base_job_name="telco-train",
-    source_dir="scripts",
-    output_path=f"s3://{bucket}/telco-trained-models/",
-    dependencies=["requirements.txt"],
-    sagemaker_session=sagemaker.Session(),
+    py_version="py3",
+    dependencies=["requirements.txt"],   # also relative to source_dir
 )
+# ---------- Run Training Job ----------
 
 
 sklearn_estimator.fit(job_name=job_name)
